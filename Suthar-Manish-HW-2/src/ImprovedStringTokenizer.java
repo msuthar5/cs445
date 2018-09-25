99708dc2247a2496bd07bfbf6210cd595ab52a57
msuthar5 <msuthar@hawk.iit.edu>
2018-09-25 18:18:14 -0500

    cleanup

--- Suthar-Manish-HW-2/src/ImprovedStringTokenizer.java
+++ Suthar-Manish-HW-2/src/ImprovedStringTokenizer.java
@@ -1,7 +1,7 @@
 import java.util.StringTokenizer;
 
 public class ImprovedStringTokenizer extends StringTokenizer {
-	
+
 	public ImprovedStringTokenizer(String str) {
 		super(str);
 	}
@@ -11,15 +11,15 @@ public class ImprovedStringTokenizer extends StringTokenizer {
 	public ImprovedStringTokenizer(String str, String delim, boolean returnDelims) {
 		super(str, delim, returnDelims);
 	}
-	
+
 	public String[] getStringAsList(){
-		
+
 		int numWords = this.countTokens();
 		String[] words = new String[numWords];
 		for (int i = 0; i < numWords; i++){
 			words[i] = this.nextToken();
 		}
-		
+
 		return(words);
 	}
 }
