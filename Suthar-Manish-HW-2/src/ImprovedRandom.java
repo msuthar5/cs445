a5661928814c03fb75709f21afede9861e557a09
msuthar5 <msuthar@hawk.iit.edu>
2018-09-25 18:06:24 -0500

    changed up range from [n,m] to [n,m)

--- Suthar-Manish-HW-2/src/ImprovedRandom.java
+++ Suthar-Manish-HW-2/src/ImprovedRandom.java
@@ -2,7 +2,7 @@ import java.util.Random;
 import java.lang.Math;
 
 public class ImprovedRandom extends Random{
-	
+
 	//Constructor for Random()
 	public ImprovedRandom(){
 		super();
@@ -11,10 +11,10 @@ public class ImprovedRandom extends Random{
 	public ImprovedRandom(long seed){
 		super(seed);
 	}
-	
+
 	public int randomBetweenRange(int min, int max){
-		return (int)((Math.random() * ((max-min) + 1)) + min); 
-		
+		return (int)((Math.random() * ((max-min))) + min); 
+
 	}
 
 }
